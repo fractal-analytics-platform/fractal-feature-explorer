28319b44d40984a0fdb8b1f175957767a243e9ac
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-06-04 09:51:06 +0200

    improve user-facing error message

--- src/fractal_explorer/authentication.py
+++ src/fractal_explorer/authentication.py
@@ -8,13 +8,13 @@ from streamlit.logger import get_logger
 logger = get_logger(__name__)
 
 
-def verify_authentication():
+def _verify_authentication():
+    logger.info("Enter _verify_authentication.")
+
     config = get_config()
 
     if config.skip_authentication:
         return
-
-    MUST_LOGIN_MESSAGE = f"Login at {config.fractal_login_url} and come back here."
 
     current_email = st.session_state.get(f"{Scope.PRIVATE}:fractal-email", None)
     current_token = st.session_state.get(f"{Scope.PRIVATE}:fractal-token", None)
@@ -22,7 +22,7 @@ def verify_authentication():
     if None not in (current_email, current_token):
         logger.info("User session is already authenticated.")
     else:
-        logger.info(f"Proceed with user authentication {config=}.")
+        logger.info("Proceed with user authentication.")
         # Extract cookie and token from browser
         try:
             cookie = next(
@@ -32,15 +32,17 @@ def verify_authentication():
             )
             token = cookie.split("=")[1]
         except StopIteration:
-            raise ValueError(
-                f"Could not find the expected cookie.\n{MUST_LOGIN_MESSAGE}"
-            )
+            msg = "Could not find the expected cookie."
+            logger.info(msg)
+            raise ValueError(msg)
         except IndexError:
-            raise ValueError(f"Invalid cookie.\n{MUST_LOGIN_MESSAGE}")
+            msg = "Invalid cookie."
+            raise ValueError(msg)
         # Get user information from Fractal backend
         logger.info("Now obtain user information.")
+        current_user_url = f"{config.fractal_backend_url}/auth/current-user/"
         response = requests.get(
-            f"{config.fractal_backend_url}/auth/current-user/",
+            current_user_url,
             headers={"Authorization": f"Bearer {token}"},
         )
         if response.ok:
@@ -50,6 +52,21 @@ def verify_authentication():
             st.session_state[f"{Scope.PRIVATE}:fractal-token"] = token
             logger.info("Obtained user information.")
         else:
-            raise ValueError(
-                f"Could not obtain Fractal user information.\n{MUST_LOGIN_MESSAGE}"
-            )
+            msg = f"Could not obtain Fractal user information from {current_user_url}."
+            logger.info(msg)
+            raise ValueError(msg)
+
+
+def verify_authentication():
+    try:
+        _verify_authentication()
+    except Exception as e:
+        logger.info(f"Authentication failed. Original error: {str(e)}.")
+        config = get_config()
+        MSG = (
+            "You are not authenticated as a Fractal user. "
+            f"Please login at {config.fractal_login_url} and "
+            "then refresh the current page."
+        )
+        st.markdown(MSG)
+        st.stop()
