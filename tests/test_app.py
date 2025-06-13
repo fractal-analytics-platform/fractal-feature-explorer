32b72d00ed164e4be2c545568d9af75149ffe922
lorenzo <lorenzo.cerrone@uzh.ch>
2025-06-13 16:36:49 +0200

    add testing config

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@
 TODO This is just a placeholder for the app testing. TBD what to test.
 
 """
+import os
 from streamlit.testing.v1 import AppTest
 
 
@@ -10,7 +11,13 @@ def test_app():
     """
     Basic Workflow Test for the Fractal Feature Explorer App.
     """
+    config_path = "tests/configs/local.toml"
+    if not os.path.exists(config_path):
+        raise FileNotFoundError("Test configuration file not found.")
+
+    os.environ["FRACTAL_FEATURE_EXPLORER_CONFIG"] = config_path
+
     app = AppTest.from_file("src/fractal_feature_explorer/main.py")
-    app.run(timeout=10)
+    app.run(timeout=15)
     
     assert not app.exception, "App raised an exception during execution."
